df054d08cede4e1e89d04318c8b141c6c4741022
Pablo Garcia <yinxro@gmail.com>
2016-05-05 13:03:08 +0200

    Attempt to implement fabrica cerrojos impl

--- dsm/FabricaCerrojosImpl.java
+++ dsm/FabricaCerrojosImpl.java
@@ -5,10 +5,19 @@ import java.util.*;
 
 public class FabricaCerrojosImpl extends UnicastRemoteObject implements FabricaCerrojos {
 
+    private Map<String, Cerrojo> cerrojos;
+
     public FabricaCerrojosImpl() throws RemoteException {
+        super();
+        cerrojos = new HashMap<>();
     }
     public synchronized	Cerrojo iniciar(String s) throws RemoteException {
-	return null;
+        Cerrojo c = cerrojos.get(s);
+        if (c == null) {
+            c = new CerrojoImpl();
+            cerrojos.put(s, c);
+        }
+        return c;
     }
 }
 
