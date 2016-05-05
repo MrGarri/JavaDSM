004479ff4275aaddbe0d9a38832360c131c88495
Pablo Garcia <yinxro@gmail.com>
2016-05-05 13:56:34 +0200

    Added state to cerrojo

--- dsm/CerrojoImpl.java
+++ dsm/CerrojoImpl.java
@@ -4,12 +4,27 @@ import java.rmi.server.*;
 
 class CerrojoImpl extends UnicastRemoteObject implements Cerrojo {
 
+    private boolean adquirido;
+
     CerrojoImpl() throws RemoteException {
+        super();
     }
 
     public synchronized void adquirir (boolean exc) throws RemoteException {
+
+        // Adquirir
+
+        adquirido = true;
+
     }
+
     public synchronized boolean liberar() throws RemoteException {
+        if(!adquirido)
+            return false;
+
+        // Liberar
+
+        adquirido = false;
         return true;
     }
 }
