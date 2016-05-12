a712260e9bc680c93880ee5fa378e58ae76d4431
Roberto Garrido <garrihendrix@gmail.com>
2016-05-12 13:50:18 +0200

    AlmacenImpl implemented without UnicastRemoteObject

--- dsm/AlmacenImpl.java
+++ dsm/AlmacenImpl.java
@@ -6,16 +6,35 @@ import java.util.*;
 
 public class AlmacenImpl extends UnicastRemoteObject implements Almacen {
 
+    private Map<String, ObjetoCompartido> contenedor;
 
     public AlmacenImpl() throws RemoteException {
+        contenedor = new HashMap<>();
     }
     public synchronized	List<ObjetoCompartido> leerObjetos(List<CabeceraObjetoCompartido> lcab)
       throws RemoteException {
 
-	return null;
+        List<ObjetoCompartido> res = new ArrayList<>();
+
+        for(CabeceraObjetoCompartido i : lcab) {
+            ObjetoCompartido oc = contenedor.get(i.getNombre());
+            if(oc != null && oc.getCabecera().getVersion() > i.getVersion()) {
+                res.add(oc);
+            }
+        }
+
+        return (res.isEmpty()) ? null : res;
     }
+
     public synchronized void escribirObjetos(List<ObjetoCompartido> loc)
      throws RemoteException  {
+
+        for(ObjetoCompartido i : loc) {
+            if(! contenedor.containsKey(i.getCabecera().getNombre())) {
+                contenedor.put(i.getCabecera().getNombre(), i);
+            }
+        }
+
     }
 }
 
